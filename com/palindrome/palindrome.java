9430e645fc45daaa386edac1f038b0eab00ad763
luqman <syed.luqman@zohocorp.com>
2019-06-04 23:56:56 +0530

    Added palindrome code

---
+++ com/palindrome/palindrome.java
@@ -0,0 +1,40 @@
+package com.palindrome;
+
+public class palindrome {
+	
+	static String newString(String s)
+	{ 
+				// length of string 
+				int l = s.length(); 
+
+				// hashing array 
+				int freq[] = new int [26] ; 
+
+				// iterate and count 
+				for (int i = 0; i < l; i++) { 
+					freq[s.charAt(i) - 'a'] += 1; 
+				}
+				// resulting string 
+				String ans = ""; 
+
+				// form the resulting string 
+				for (int i = 0; i < 26; i++) {
+
+				// number of times character appears 
+					for (int j = 0; j < freq[i]; j++) { 
+
+					// append to resulting string 
+						ans += (char)(97 + i); 
+					}
+				}
+
+				return ans;
+				}
+	
+	public static void main(String args[]) 
+		{
+				String s = "aabab"; 
+				System.out.println(newString(s));
+		}
+
+} 
